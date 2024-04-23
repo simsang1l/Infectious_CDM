bc1642de1b19d7d911810d5ee1b7047ab0cb6cff
심상일 <simsang1db@gmail.com>
2024-04-23 11:15:19 +0900

    add metadata_check, update excel_util 품질진단지표 config main_qc for metadata QC

--- JBUH/main_qc.py
+++ JBUH/main_qc.py
@@ -10,6 +10,9 @@ from QC.src import (table_person_row_count, table_provider_row_count,
                     field_measurement_summary, field_procedure_occurrence_summary,
                     field_observation_period_summary, field_local_edi_summary
                     )
+from QC.src.dq_check import *
+from QC.src.metadata_check import *
+from QC.src.excel_util import get_sheet_row_count
 
 def load_config(config_path):
     with open(config_path, 'r', encoding="utf-8") as file:
@@ -23,6 +26,8 @@ if __name__ == "__main__":
     excel_path = config["excel_path"]
     sheetname_table = config["sheet_table_count"]
     sheetname_field = config["sheet_field_summary"]
+    sheetname_dq = config["sheet_dq"]
+    sheetname_meta = config["sheet_meta"]
 
     ### table 품질 진단 ###
     # table_person_row_count.person_row_count(config, cdm_path, source_path, excel_path)
@@ -47,4 +52,19 @@ if __name__ == "__main__":
     # field_measurement_summary.measurement_field_summary(cdm_path, excel_path, sheetname_field)
     # field_procedure_occurrence_summary.procedure_occurrence_field_summary(cdm_path, excel_path, sheetname_field)
     # field_observation_period_summary.observation_period_field_summary(cdm_path, excel_path, sheetname_field)
-    field_local_edi_summary.local_edi_field_summary(cdm_path, excel_path, sheetname_field)
+    # field_local_edi_summary.local_edi_field_summary(cdm_path, excel_path, sheetname_field)
+
+    ### DQ 진단 ###
+    # length = 50
+    # for i in range(length):
+    #     func_name = f"DQ_{str(i+1).zfill(4)}"
+    #     func = globals()[func_name]
+    #     func(cdm_path, excel_path, sheetname_dq)
+
+    ### METADATA 진단 ###
+    length = get_sheet_row_count(excel_path, sheetname_meta) - 1
+    print(length)
+    for i in range(23, length):
+        func_name = f"META_{str(i+1).zfill(4)}"
+        func = globals()[func_name]
+        func(config, cdm_path, excel_path, sheetname_meta)
